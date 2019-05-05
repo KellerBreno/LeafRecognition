9d18c1b7880437dd1767519396bddc761cb63942
KellerBreno <brenok.private@gmail.com>
2019-05-05 14:40:13 -0300

    Added extraction step

---
+++ src/Extraction.py
@@ -0,0 +1,34 @@
+import os
+
+from numpy import uint8
+from skimage import io
+from skimage.color import rgb2gray
+from skimage.filters import threshold_otsu
+from skimage.measure import label, regionprops
+
+
+def process_all_images(path, path_result):
+    files = []
+    for r, d, f in os.walk(path):
+        for file in f:
+            if ".png" in file:
+                files.append(os.path.join(r, file))
+
+    output_file = open(path_result, "w+")
+    output_file.write("filename;class;area;convex_area;eccentricity;filled_area;perimeter;solidity\n")
+    for f in files:
+        print(f)
+        img = io.imread(f)
+        thresh = threshold_otsu(rgb2gray(img))
+        binary = rgb2gray(img) > thresh
+        regions = regionprops(uint8(binary))
+        for region in regions:
+            segments = f.split("\\")
+            filename = segments[len(segments) - 1]
+            classname = segments[len(segments) - 2]
+            output_file.write(str(
+                filename + ";" + classname + ";" + str(region.area) + ";" + str(region.convex_area) + ";" + str(
+                    region.eccentricity) + ";" + str(region.filled_area) + ";" + str(region.perimeter) + ";" + str(
+                    region.solidity) + "\n"))
+            output_file.flush()
+    output_file.close()
--- src/Main.py
+++ src/Main.py
@@ -1,11 +1,18 @@
-from src import Mapping
+from src import Mapping, Extraction
 
 if __name__ == "__main__":
+    # Map namefiles from kaggle in dataset
     # Mapping.map("D:\\Projects\\LeafRecognition\\data\\Original\\test",
     #             "D:\\Projects\\LeafRecognition\\data\\Nonsegmented")
+
+    # Rename segmentated images using the previous map
     # Mapping.rewrite_train_data("D:\\Projects\\LeafRecognition\\data\\gabarito\\ScentlessMayweed.csv",
     #                            "D:\\Projects\\LeafRecognition\\data\\Segmented\\Scentless Mayweed",
     #                            "D:\\Projects\\LeafRecognition\\data\\NewData\\train\\Scentless Mayweed")
-    Mapping.rewrite_test_data("D:\\Projects\\LeafRecognition\\data\\gabarito\\test.csv",
-                              "D:\\Projects\\LeafRecognition\\data\\Segmented",
-                              "D:\\Projects\\LeafRecognition\\data\\NewData\\test")
+    # Mapping.rewrite_test_data("D:\\Projects\\LeafRecognition\\data\\gabarito\\test.csv",
+    #                           "D:\\Projects\\LeafRecognition\\data\\Segmented",
+    #                           "D:\\Projects\\LeafRecognition\\data\\NewData\\test")
+
+    # Extract data from images
+    Extraction.process_all_images("D:\\Projects\\LeafRecognition\\data\\NewData\\train",
+                                  "D:\\Projects\\LeafRecognition\\data\\train_data.csv")
